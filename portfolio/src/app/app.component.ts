abdc942dabceeb36c4a6c0516cfc9ed619f217cb
SumukhP-dev <spaspuleti3@gatech.edu>
2025-01-17 04:02:07 -0500

    Add cards for each project

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../my-node-backend/api.service';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CoreModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
