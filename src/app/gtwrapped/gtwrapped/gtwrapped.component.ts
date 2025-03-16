0baefa58e98deb79aaf720f69678b27aa7307925
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-03-16 19:15:41 -0400

    Add images and pages of nextjsdashboard website

--- src/app/gtwrapped/gtwrapped/gtwrapped.component.ts
+++ src/app/gtwrapped/gtwrapped/gtwrapped.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarouselModule,
+  NgbCarousel,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gtwrapped',
-  imports: [],
-  templateUrl: './gtwrapped.component.html'
+  imports: [NgbCarouselModule, FormsModule],
+  templateUrl: './gtwrapped.component.html',
 })
 export class GtwrappedComponent {
+  images = [
+    '/images/gtwrapped/InsightsPage.png',
+    '/images/gtwrapped/LoginPage.png',
+    '/images/gtwrapped/MapPage.png',
+    '/images/gtwrapped/FigmaInsightsPage.png',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
--- src/app/weathernow/weathernow.component.ts
+++ src/app/weathernow/weathernow.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarouselModule,
+  NgbCarousel,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-weathernow',
-  imports: [],
-  templateUrl: './weathernow.component.html'
+  imports: [NgbCarouselModule, FormsModule],
+  templateUrl: './weathernow.component.html',
 })
 export class WeathernowComponent {
+  images = [
+    '/images/weathernow/main-weather-page.png',
+    '/images/weathernow/contact-us-page.png',
+    '/images/weathernow/forecast-page.png',
+    '/images/weathernow/figma-forecast-page.png',
+    '/images/weathernow/figma-heatmap-page.png',
+    '/images/weathernow/figma-login-page.png',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
