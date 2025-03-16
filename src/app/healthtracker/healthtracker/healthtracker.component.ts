e66df9f0a029d11f0aafc4d76b500bc223922135
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-03-16 07:33:17 -0400

    Add images and download link on healthtracker page

--- src/app/healthtracker/healthtracker/healthtracker.component.ts
+++ src/app/healthtracker/healthtracker/healthtracker.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarousel,
+  NgbCarouselModule,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-healthtracker',
-  imports: [],
-  templateUrl: './healthtracker.component.html'
+  imports: [NgbCarouselModule, FormsModule],
+  templateUrl: './healthtracker.component.html',
 })
 export class HealthtrackerComponent {
+  images = [
+    '/images/healthtracker/Sprite.jpeg',
+    '/images/healthtracker/LoginPage.png',
+    '/images/healthtracker/PersonalinfoPage.jpeg',
+    '/images/healthtracker/Calorie.jpeg',
+    '/images/healthtracker/WorkoutPlans.jpeg',
+    '/images/healthtracker/WorkoutTrackerPage.jpeg',
+    '/images/healthtracker/CommunityPage.jpeg',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
