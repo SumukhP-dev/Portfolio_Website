146b8c01367ce04c6f9e3c501d19c9483bc051b7
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-03-16 01:27:04 -0400

    Add nextjsdashboard images

--- src/app/nextjsdashboard/nextjsdashboard/nextjsdashboard.component.ts
+++ src/app/nextjsdashboard/nextjsdashboard/nextjsdashboard.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarouselModule,
+  NgbCarousel,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nextjsdashboard',
-  imports: [],
-  templateUrl: './nextjsdashboard.component.html'
+  imports: [NgbCarouselModule, FormsModule],
+  templateUrl: './nextjsdashboard.component.html',
 })
 export class NextjsdashboardComponent {
+  images = [
+    '/images/nextjsdashboard/intro-sample-finance-website-vercel-app.png',
+    '/images/nextjsdashboard/login-sample-finance-website-vercel-app.png',
+    '/images/nextjsdashboard/dashboard-sample-finance-website-vercel-app.png',
+    '/images/nextjsdashboard/create-invoices-sample-finance-website-vercel-app.png',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "selector",
+  allowJs: true,
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
