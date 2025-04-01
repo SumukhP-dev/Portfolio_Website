1e804dd9627985ad64f4dc80864fd85d004710e0
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-04-01 08:45:31 -0400

    Add start component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,34 +16,7 @@ import { OnInit, OnDestroy } from '@angular/core';
 import { selectWrapper, toggleWrapper, wrapperFeature } from '../app.state';
 
 @Component({
-  selector: 'app-home',
-  imports: [
-    RouterModule,
-    CoreModule,
-    HostComponent,
-    DropdownDirective,
-    CommonModule,
-  ],
+  imports: [],
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-  store: Store<{ wrapper: boolean }>;
-  cRef: ChangeDetectorRef;
-  wrapper$: Observable<boolean> | undefined;
-  wrapper: any;
-
-  constructor(store: Store<{ wrapper: boolean }>, cRef: ChangeDetectorRef) {
-    this.store = store;
-    this.cRef = cRef;
-  }
-  ngOnInit(): void {
-    this.wrapper$ = this.store.select(selectWrapper);
-
-    setTimeout(() => {
-      this.wrapper$ = this.store.select(selectWrapper);
-      this.wrapper$.subscribe((value) => {
-        console.log(1 + ' ' + value);
-      });
-    }, 3000);
-  }
-}
+export class HomeComponent {}
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,49 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  model,
+  SimpleChanges,
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { CoreModule } from '../core/core.module';
+import { DropdownDirective } from '../core/features/dropdown/dropdown.directive';
+import { HostComponent } from '../core/features/host/host.component';
+import { ChangeDetectorRef } from '@angular/core';
+import { OnInit, OnDestroy } from '@angular/core';
+import { selectWrapper, toggleWrapper, wrapperFeature } from '../app.state';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    RouterModule,
+    CoreModule,
+    HostComponent,
+    DropdownDirective,
+    CommonModule,
+  ],
+  templateUrl: './start.component.html',
+})
+export class StartComponent implements OnInit {
+  store: Store<{ wrapper: boolean }>;
+  cRef: ChangeDetectorRef;
+  wrapper$: Observable<boolean> | undefined;
+  wrapper: any;
+
+  constructor(store: Store<{ wrapper: boolean }>, cRef: ChangeDetectorRef) {
+    this.store = store;
+    this.cRef = cRef;
+  }
+  ngOnInit(): void {
+    this.wrapper$ = this.store.select(selectWrapper);
+
+    setTimeout(() => {
+      this.wrapper$ = this.store.select(selectWrapper);
+      this.wrapper$.subscribe((value) => {
+        console.log(value);
+      });
+    }, 3000);
+  }
+}
