8619c27c36b8d000aba598567b5833cfdf1e8e0c
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-08-03 13:50:06 -0400

    Add game dev projects

--- src/app/game-dev/averys-adventure/averys-adventure.component.ts
+++ src/app/game-dev/averys-adventure/averys-adventure.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarousel,
+  NgbCarouselModule,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-averysadventure',
-  imports: [],
-  templateUrl: './averysadventure.component.html'
+  imports: [NgbCarouselModule, FormsModule],
+  templateUrl: './averys-adventure.component.html',
 })
 export class AverysadventureComponent {
+  images = [
+    '/images/averys-adventure/StartImage.png',
+    '/images/averys-adventure/BeginningImage.png',
+    '/images/averys-adventure/MidwayImage.png',
+    '/images/averys-adventure/WinImage.png',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
--- src/app/internet-tech-dev/apache-tomcat-web-application-server/apache-tomcat-web-application-server.component.ts
+++ src/app/internet-tech-dev/apache-tomcat-web-application-server/apache-tomcat-web-application-server.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarousel,
+  NgbCarouselModule,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-apache-tomcat-web-application-server',
-  imports: [],
+  imports: [NgbCarouselModule, FormsModule],
   templateUrl: './apache-tomcat-web-application-server.component.html',
-  styleUrl: './apache-tomcat-web-application-server.component.scss'
+  styleUrl: './apache-tomcat-web-application-server.component.scss',
 })
 export class ApacheTomcatWebApplicationServerComponent {
+  images = [
+    '/images/',
+    '/images/healthtracker/LoginPage.png',
+    '/images/healthtracker/PersonalinfoPage.jpeg',
+    '/images/healthtracker/Calorie.jpeg',
+    '/images/healthtracker/WorkoutPlans.jpeg',
+    '/images/healthtracker/WorkoutTrackerPage.jpeg',
+    '/images/healthtracker/CommunityPage.jpeg',
+    '/images/healthtracker/database-console-firebase.png',
+    '/images/healthtracker/database-console-firebase-2.png',
+    '/images/healthtracker/database-console-firebase-3.png',
+  ];
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true })
+  carousel: NgbCarousel = new NgbCarousel();
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused();
+    }
+  }
 }
