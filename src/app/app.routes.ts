9683820ca3a84fc6de4e80c86b2df22a4d7c9cf2
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-08-15 18:11:02 -0400

    Delete resume header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,20 @@ import { AherosjourneyComponent } from './game-dev/a-heros-journey/a-heros-journ
 import { AverysadventureComponent } from './game-dev/averys-adventure/averys-adventure.component';
 import { BallrollingComponent } from './game-dev/ball-rolling/ball-rolling.component';
 import { HomeComponent } from './home/home.component';
-import { ResumeComponent } from './resume/resume.component';
 import { StartComponent } from './start/start.component';
 import { GtwrappedComponent } from './web-app-dev/gt-wrapped/gt-wrapped.component';
 import { HealthtrackerComponent } from './web-app-dev/health-tracker/health-tracker.component';
 import { MyMoneyTrackerComponent } from './web-app-dev/my-money-tracker/my-money-tracker.component';
 import { NextjsdashboardComponent } from './web-app-dev/nextjs-dashboard/nextjs-dashboard.component';
 import { WeathernowComponent } from './web-app-dev/weather-now/weather-now.component';
-import { ApacheTomcatWebApplicationServerComponent } from './internet-tech-dev/apache-tomcat-web-application-server/apache-tomcat-web-application-server.component';
-import { GtmobisimMobileTraceGenerationComponent } from './internet-tech-dev/gtmobisim-mobile-trace-generation/gtmobisim-mobile-trace-generation.component';
-import { NutchWebCrawlerWithMemcachedserverComponent } from './internet-tech-dev/nutch-web-crawler-with-memcachedserver/nutch-web-crawler-with-memcachedserver.component';
-import { VotingLedgerFabricApplicationComponent } from './internet-tech-dev/voting-ledger-fabric-application/voting-ledger-fabric-application.component';
+import { HealthcarePolicyImpactTrackerComponent } from './web-app-dev/healthcare-policy-impact-tracker/healthcare-policy-impact-tracker.component';
+import { ApacheTomcatWebApplicationServerComponent } from './web-server-crawler-dev/apache-tomcat-web-application-server/apache-tomcat-web-application-server.component';
+import { GtmobisimMobileTraceGenerationComponent } from './web-server-crawler-dev/gtmobisim-mobile-trace-generation/gtmobisim-mobile-trace-generation.component';
+import { NutchWebCrawlerWithMemcachedserverComponent } from './web-server-crawler-dev/nutch-web-crawler-with-memcachedserver/nutch-web-crawler-with-memcachedserver.component';
+import { VotingLedgerFabricApplicationComponent } from './web-server-crawler-dev/voting-ledger-fabric-application/voting-ledger-fabric-application.component';
+
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'resume', component: ResumeComponent },
   { path: 'mymoneytracker', component: MyMoneyTrackerComponent },
   { path: 'gtwrapped', component: GtwrappedComponent },
   { path: 'healthtracker', component: HealthtrackerComponent },
@@ -27,6 +27,10 @@ export const routes: Routes = [
   { path: 'aherosjourney', component: AherosjourneyComponent },
   { path: 'ballrolling', component: BallrollingComponent },
   { path: 'averysadventure', component: AverysadventureComponent },
+  {
+    path: 'healthcarepolicyimpacttracker',
+    component: HealthcarePolicyImpactTrackerComponent,
+  },
   { path: 'carlaaiautopilot', component: CarlaAiAutopilotComponent },
   {
     path: 'apachetomcatwebapplicationserver',
